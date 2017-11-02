a70b0648c71e7ee0be8c7031e5a28745833253ef
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-11-02 18:21:44 +0400

    up

--- src/main/java/io/project/app/HazelcastConfig.java
+++ src/main/java/io/project/app/HazelcastConfig.java
@@ -3,10 +3,11 @@ package io.project.app;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.JoinConfig;
 import com.hazelcast.config.MaxSizeConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-
+import static java.util.Collections.singletonList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,18 +15,28 @@ import org.springframework.context.annotation.Configuration;
 public class HazelcastConfig {
 
     @Bean
+    public Config config() {
+        Config config = new Config();
+        JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+        joinConfig.getMulticastConfig().setEnabled(false);
+        joinConfig.getTcpIpConfig().setEnabled(true).setMembers(singletonList("127.0.0.1"));
+        return config;
+    }
+
+    @Bean
     public HazelcastInstance hazelcastInstance() {
         Config config = new Config();
+        config.setProperty("hazelcast.jmx", "true");
         LocalListConfig listConfig = new LocalListConfig();
         listConfig.setMaxSize(50);
         listConfig.setTimeToLiveSeconds(10);
         listConfig.setBackupCount(0);
         listConfig.setName("contactList");
-        
+
         config.addListConfig(listConfig);
 
         ContactMapConfig contactMapConfig = new ContactMapConfig();
-        
+
         contactMapConfig.setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE));
         contactMapConfig.setTimeToLiveSeconds(10);
         contactMapConfig.setBackupCount(0);
@@ -38,9 +49,10 @@ public class HazelcastConfig {
         config.addMapConfig(contactMapConfig);
 
         HazelcastInstance instance = Hazelcast.newHazelcastInstance(config);
-        
+
         return instance;
     }
+    
 }
 
 
